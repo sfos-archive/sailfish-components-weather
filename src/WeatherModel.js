f9150b4ba39a326f9f956282ddde44965cd121cf
Pekka Vuorela <pekka.vuorela@jollamobile.com>
2016-10-11 14:34:29 +0300

    [components-weather] Map high clouds and fog to existing graphics. Contributes to JB#36534

--- src/WeatherModel.js
+++ src/WeatherModel.js
@@ -141,6 +141,11 @@ function weatherType(code) {
     var precipirationType = code.charAt(3)
 
     var type
+    // FIXME: need graphics for thin high clouds
+    if (cloudiness == '5') {
+        cloudiness = '1'
+    }
+
     switch(precipirationRate) {
     case '0':
         switch (cloudiness) {
@@ -153,6 +158,7 @@ function weatherType(code) {
             type = "cloud-3"
             break
         case '4':
+        case '6': // FIXME: need graphics for fog
             type = "cloud-4"
             break
         default:
